058b7d292495ae4be976c28f31361280784b2cb4
lkalicki <lkalicki98@gmail.com>
2021-10-22 13:11:08 -0400

    create task element

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,30 @@ var createTaskHandler = function(event) {
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
 
+// package up data as an object
+var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+    
+};
+
+var createTaskEl = function(taskDataObj) {
+// create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
 // create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
-
-// add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
-    
-};
+}
 
 formEl.addEventListener("submit", createTaskHandler);
